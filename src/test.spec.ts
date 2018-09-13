44bcf04fa755e7b8ae472bb6622059385b6472b9
awaw00 <awaw0618@outlook.com>
2018-09-13 17:07:25 +0800

    add test

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,8 +1,19 @@
-import { RxStore, typeDef, asyncTypeDef, AsyncActionType, ActionType } from './index';
+import { Container, inject, injectable, postConstruct } from 'inversify';
+import { ActionStream, ActionType, AsyncActionType, asyncTypeDef, RxStore, typeDef } from './index';
 import { expect } from 'chai';
-import { postConstruct } from 'inversify';
+import { combineLatest, Subject } from 'rxjs';
+import { map, skip } from 'rxjs/operators';
 
 describe('Test', () => {
+  let rootContainer: Container;
+  beforeEach(() => {
+    rootContainer = new Container();
+    rootContainer.bind(ActionStream).toConstantValue(new Subject());
+  });
+
+  afterEach(() => {
+  });
+
   it('Should typeDef and asyncTypeDef work', () => {
     class Store extends RxStore<any> {
       @typeDef public TYPE!: ActionType;
@@ -21,5 +32,86 @@ describe('Test', () => {
     expect(typeof store.ASYNC_TYPE.START).is.eq('symbol');
     expect(typeof store.ASYNC_TYPE.END).is.eq('symbol');
     expect(typeof store.ASYNC_TYPE.END).is.eq('symbol');
+  });
+  it('Store combination', (done) => {
+    interface ChildState {
+      name: string;
+      age: number;
+    }
+
+    interface ParentState {
+      parentName: string;
+      child: ChildState;
+    }
+
+    @injectable()
+    class ChildStore extends RxStore<ChildState> {
+      @typeDef public CHANGE_AGE!: ActionType;
+
+      @postConstruct()
+      private storeInit () {
+        this.init({
+          initialState: {
+            name: 'child',
+            age: 10
+          },
+          reducer: (state, action) => {
+            switch (action.type) {
+              case this.CHANGE_AGE:
+                return {...state, age: 20};
+            }
+
+            return state;
+          }
+        });
+      }
+    }
+
+    @injectable()
+    class ParentStore extends RxStore<ParentState> {
+
+      @inject(ChildStore)
+      private childStore!: ChildStore;
+
+      @postConstruct()
+      private storeInit () {
+        this.init({
+          initialState: {
+            parentName: 'parent',
+            child: this.childStore.options.initialState
+          },
+          reducer: (state, action) => {
+            return state;
+          }
+        });
+
+        this.state$ = combineLatest(
+          this.state$,
+          childStore.state$
+        ).pipe(
+          map(([thisState, childState]) => {
+            return {
+              ...thisState,
+              child: childState
+            };
+          })
+        );
+      }
+    }
+
+    rootContainer.bind(ChildStore).toSelf().inSingletonScope();
+    rootContainer.bind(ParentStore).toSelf().inSingletonScope();
+
+    const childStore = rootContainer.get(ChildStore);
+    const parentStore = rootContainer.get(ParentStore);
+
+    parentStore.state$.pipe(
+      skip(1)
+    ).subscribe(state => {
+      expect(state.child.age).to.eq(20);
+      done();
+    });
+
+    childStore.dispatch({type: childStore.CHANGE_AGE});
   });
 });
